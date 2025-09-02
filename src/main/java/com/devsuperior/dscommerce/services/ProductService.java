13b763baabf59b50bf0d234a9f96cf5a1af1a0f8
Matheus <matheusbarrosohomeoffice@gmail.com>
2025-09-02 15:17:35 -0300

    GET product by id

---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dscommerce.services;
+
+
+import com.devsuperior.dscommerce.DTO.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+    }
+}
