70862abce83f2a0df29e4b2f3d2f8d59d5338291
Matheus <matheusbarrosohomeoffice@gmail.com>
2025-09-03 16:47:15 -0300

    Exception handling

--- src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
@@ -9,8 +9,7 @@ public class ProductDTO {
     private Double price;
     private String imgUrl;
 
-    public ProductDTO(){
-    }
+    public ProductDTO() {}
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
